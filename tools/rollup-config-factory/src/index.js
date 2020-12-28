65557765dcf5fad0de4e88e10472f45dba8a76a2
chenshaorui <617672431@qq.com>
2020-12-28 11:33:44 +0800

    chore(rollup-config-factory): use Rollup TypeScript plugin

--- tools/rollup-config-factory/src/index.js
+++ tools/rollup-config-factory/src/index.js
@@ -1,15 +1,16 @@
 const { babel } = require("@rollup/plugin-babel");
+const typescript = require("@rollup/plugin-typescript");
 
 const getRollupConfig = () => {
   return {
-    input: "src/index.js",
+    input: "src/index.ts",
     output: [
       {
         file: "dist/index.bundle.js",
         format: "umd",
       },
     ],
-    plugins: [babel()],
+    plugins: [typescript(), babel()],
   };
 };
 
